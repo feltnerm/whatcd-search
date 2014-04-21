d63680bf206ef0f7bd6a310d663389827edf504e
Mark Feltner <mark@feltner.me>
2014-04-21 08:51:36 -0500

    feat: Return promise,and allow optional callbacks
    All major functions return promises **or** callbacks. Using Promise over RSVP.

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 #!/usr/bin/env node
 
-var _ = require('underscore'),
+var util = require('util'),
+    _ = require('underscore'),
     minimist = require('minimist');
 
-var search = require('./lib/search');
+var Client = require('./lib/search');
 
 // set default params
 var params = {
-    media: 'CD',
-    encoding: 'lossless',
-    format: 'FLAC',
+    searchstr: 'Pure Guava'
 };
+
+function filter_torrents(torrents){
+    return torrents.map(function (torrent) {
+        var info = _.extend(
+            _.omit(torrent.group, 'wikiBody', 'vanityHouse',
+                   'isBookmarked'),
+            _.omit(torrent.torrent, 'description', 'seeders',
+                   'leechers', 'snatched', 'freeTorrent',
+                   'reported', 'time', 'description', 'fileList')
+        );
+
+        return info;
+    });
+}
 
 if (process.argv.length > 2) {
     var args = process.argv.slice(2),
         argv = minimist(args);
 
     params = _.extend(params, argv);
-    console.log(search(params));
+
+    var client = Client(process.env.WHAT_USERNAME, process.env.WHAT_PASSWORD);
+
+    client.search(params).then(function(result){
+        console.dir(filter_torrents(result));
+    }, function(error){
+        debug(error);
+    });
 
 } else {
     console.log("Usage: \n" +
@@ -29,6 +49,6 @@ if (process.argv.length > 2) {
                 "--format [FLAC,...]\n" +
                 "\n" +
                 " .. and so forth ...\n");
-    process.exit(1);
+    process.exit(1)
 
 }
